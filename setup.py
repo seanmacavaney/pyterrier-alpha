fd4cf62e79eda188609305f42b346a3fbcc12e19
Sean MacAvaney <sean.macavaney@gmail.com>
2024-07-23 12:51:23 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyterrier-alpha",
-    version="0.2.1",
+    version="0.2.2",
     author="Sean MacAvaney",
     author_email='sean.macavaney@glasgow.ac.uk',
     description="Alpha channel of features for PyTerrier",
