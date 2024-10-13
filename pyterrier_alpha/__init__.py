56ac5aa4e29b7ec38440c679cc33bfcb27bc725a
Sean MacAvaney <sean.macavaney@gmail.com>
2024-10-13 13:56:56 +0100

    ArtifactBuilder

--- pyterrier_alpha/__init__.py
+++ pyterrier_alpha/__init__.py
@@ -3,16 +3,17 @@
 Functionality provided by this package is subject to change in future versions.
 """
 
-__version__ = '0.9.0'
+__version__ = '0.10.0'
 
 from pyterrier_alpha import artifact, colab, inspect, io, utils, validate
-from pyterrier_alpha.artifact import Artifact
+from pyterrier_alpha.artifact import Artifact, ArtifactBuilder
 from pyterrier_alpha.dataframe_builder import DataFrameBuilder
 from pyterrier_alpha.rbo import RBO, rbo
 from pyterrier_alpha.transformer_repr import transformer_repr
 
 __all__ = [
     'Artifact',
+    'ArtifactBuilder',
     'DataFrameBuilder',
     'RBO',
     'artifact',
--- pyterrier_alpha/artifact.py
+++ pyterrier_alpha/artifact.py
@@ -1,5 +1,4 @@
 """PyTerrier artifact module."""
-
 import contextlib
 import io
 import json
@@ -7,6 +6,8 @@ import os
 import sys
 import tarfile
 import tempfile
+from contextlib import contextmanager
+from enum import Enum
 from hashlib import sha256
 from pathlib import Path
 from typing import Any, Dict, Iterator, Optional, Tuple, Union
@@ -523,3 +524,83 @@ def _hf_url_resolver(parsed_url: ParseResult) -> str:
     if '@' in org_repo:
         org_repo, ref = org_repo.split('@', 1)
     return f'https://huggingface.co/datasets/{org_repo}/resolve/{ref}/artifact.tar.lz4'
+
+
+class ArtifactBuilderMode(Enum):
+    """Enumeration of artifact builder modes."""
+    create = 'create'
+    overwrite = 'overwrite'
+    append = 'append'
+
+
+class ArtifactBuilder:
+    """Context manager for building artifacts."""
+    def __init__(
+        self,
+        artifact: Optional[Artifact] = None,
+        *,
+        mode: Union[ArtifactBuilderMode, str] = 'create',
+        path: Optional[Union[str, Path]] = None,
+        type: Optional[str] = None,
+        format: Optional[str] = None,
+        package_hint: Optional[str] = None,
+    ) -> None:
+        """Context manager for building artifacts."""
+        if artifact is None:
+            assert path is not None, "path must be provided if artifact isn't"
+            assert type is not None, "type must be provided if artifact isn't"
+            assert format is not None, "format must be provided if artifact isn't"
+            assert package_hint is not None, "package_hint must be provided if artifact isn't"
+
+        self.mode = ArtifactBuilderMode(mode)
+        self.path = Path(path or artifact.path)
+
+        if type is None or format is None:
+            type, format = pta.inspect.artifact_type_format(artifact)
+
+        if package_hint is None:
+            if hasattr(artifact, 'ARTIFACT_PACKAGE_HINT'):
+                package_hint = artifact.ARTIFACT_PACKAGE_HINT
+            else:
+                package_hint = artifact.__class__.__module__.split('.')[0]
+
+        self.metadata = {
+            'type': type,
+            'format': format,
+            'package_hint': package_hint,
+        }
+
+    def __enter__(self) -> 'ArtifactBuilder':
+        # TODO: check if the path exists and either:
+        if self.mode == ArtifactBuilderMode.create:
+            if self.path.exists():
+                raise FileExistsError(f'{str(self.path)} already exists.')
+            self.path.mkdir(parents=True)
+        elif self.mode == ArtifactBuilderMode.overwrite:
+            if self.path.exists():
+                raise NotImplementedError()
+                # This might be a bit tricky... Do we recursively delete the entire
+                # directory if it exists? What if the user accidently enters a path
+                # such as '/'? Do we need some sort of check or warning if the path
+                # is very large? Hmmm...
+            else:
+                self.path.mkdir(parents=True)
+        elif self.mode == ArtifactBuilderMode.append:
+            raise NotImplementedError()
+
+        return self
+
+    def __exit__(self, exc_type: Any, exc_value: Any, traceback: Any) -> None:
+        if exc_type is not None:
+            # TODO clean up
+            pass
+        else:
+            # Log the artifact metadata
+            meta_path = self.path / 'pt_meta.json'
+            try:
+                with open(meta_path, 'wt') as fout:
+                    json.dump(self.metadata, fout)
+            except:
+                if meta_path.exists():
+                    meta_path.unlink()
+                raise
