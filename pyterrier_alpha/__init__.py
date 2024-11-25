c60c5c470e3dfcd0bbb32666277b9ae0cfeb8def
Sean MacAvaney <sean.macavaney@gmail.com>
2024-11-25 14:51:36 +0000

    export ArtifactBuilderMode to top level package

--- pyterrier_alpha/__init__.py
+++ pyterrier_alpha/__init__.py
@@ -3,10 +3,10 @@
 Functionality provided by this package is subject to change in future versions.
 """
 
-__version__ = '0.9.3'
+__version__ = '0.9.4'
 
 from pyterrier_alpha import artifact, colab, inspect, io, utils, validate
-from pyterrier_alpha.artifact import Artifact, ArtifactBuilder
+from pyterrier_alpha.artifact import Artifact, ArtifactBuilder, ArtifactBuilderMode
 from pyterrier_alpha.dataframe_builder import DataFrameBuilder
 from pyterrier_alpha.rbo import RBO, rbo
 from pyterrier_alpha.transformer_repr import transformer_repr
@@ -14,6 +14,7 @@ from pyterrier_alpha.transformer_repr import transformer_repr
 __all__ = [
     'Artifact',
     'ArtifactBuilder',
+    'ArtifactBuilderMode',
     'DataFrameBuilder',
     'RBO',
     'artifact',
