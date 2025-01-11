e0cb5043d6aad88ff2f3487b3a71911ad27ababe
Sean MacAvaney <sean.macavaney@gmail.com>
2025-01-11 14:31:43 +0000

    refactor

--- pyterrier_alpha/transform.py
+++ pyterrier_alpha/transform.py
@@ -2,9 +2,11 @@
 
 import functools
 from typing import Callable, Dict, Iterable, Optional, Union
+from warnings import warn
 
 import pandas as pd
 import pyterrier as pt
+from packaging.version import Version
 
 T_TRANSFORM_FN = Callable[[pd.DataFrame], pd.DataFrame]
 T_TRANSFORM_ITER_FN = Callable[[Iterable[Dict]], Iterable[Dict]]
@@ -36,22 +38,22 @@ def by_query(*,
     .. versionchanged:: 0.12.0 added support for ``transform_iter``
     .. versionchanged:: 0.12.1 supports verbose kwarg
     """
-    from packaging.version import Version
-    kwargs = {'add_ranks': add_ranks,
-                'batch_size' : batch_size,
-                'iter' : False,
-    }
-    if verbose and Version(pt.__version__) >= Version('0.12.1'):
-        kwargs['verbose'] = verbose
     def _wrapper(fn: Union[T_TRANSFORM_FN]) -> Union[T_TRANSFORM_FN]:
         is_iter = fn.__name__ == 'transform_iter'
         if is_iter:
             assert not add_ranks, "add_ranks not supported for by_query with transform_iter; set add_ranks=False"
             @functools.wraps(fn)
             def _transform_iter(self: pt.Transformer, inp: Iterable[Dict]) -> Iterable[Dict]:
+                kwargs = {}
+                if Version(pt.__version__) >= Version('0.12.1'):
+                    kwargs['verbose'] = verbose
+                elif verbose:
+                    warn(f'verbose ignored for pyterrier version {pt.__version__} (minimum 0.12.1 required)')
                 return pt.apply.by_query(
                     functools.partial(fn, self),
-                    **kwargs
+                    batch_size=batch_size,
+                    iter=True,
+                    **kwargs,
                 )(inp)
             return _transform_iter
         else:
@@ -59,7 +61,10 @@ def by_query(*,
             def _transform(self: pt.Transformer, inp: pd.DataFrame) -> pd.DataFrame:
                 return pt.apply.by_query(
                     functools.partial(fn, self),
-                    **kwargs,
+                    add_ranks=add_ranks,
+                    batch_size=batch_size,
+                    iter=False,
+                    verbose=verbose,
                 )(inp)
             return _transform
     return _wrapper
