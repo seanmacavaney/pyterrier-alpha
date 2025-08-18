8b85655cdf93cf74a9cc34edf92bac250c2927f8
Sean MacAvaney <sean.macavaney@gmail.com>
2025-08-18 15:56:31 +0100

    use pt.inspect for transformer_repr

--- pyterrier_alpha/transformer_repr.py
+++ pyterrier_alpha/transformer_repr.py
@@ -3,8 +3,10 @@
 import inspect
 from typing import Any
 
+import pyterrier as pt
 
-def transformer_repr(self: Any) -> str:
+
+def transformer_repr(transformer: Any) -> str:
     """Return a string representation of a transformer instance.
 
     .. versionadded:: 0.3.0
@@ -14,21 +16,18 @@ def transformer_repr(self: Any) -> str:
 
     .. versionchanged:: 0.12.1
         Ignore verbose
+
+    .. versionchanged:: TODO
+        Use :meth:`pt.inspect.transformer_attributes`
     """
-    cls = self.__class__
-    init = self.__init__
-    signature = inspect.signature(init)
+    cls = transformer.__class__
     mode = 'pos'
     args = []
-    for p in signature.parameters.values():
-        if p.kind not in (inspect.Parameter.POSITIONAL_ONLY, inspect.Parameter.POSITIONAL_OR_KEYWORD):
+    for attr in pt.inspect.transformer_attributes(transformer):
+        if attr.init_parameter_kind not in (inspect.Parameter.POSITIONAL_ONLY, inspect.Parameter.POSITIONAL_OR_KEYWORD):
             mode = 'kwd'
-        try:
-            val = getattr(self, f'_{p.name}')
-        except AttributeError:
-            val = getattr(self, p.name)
-        if val != p.default and p.name != 'verbose':
-            args.append(f'{p.name}={val!r}' if mode == 'kwd' else repr(val))
+        if attr.value != attr.init_default_value and attr.name != 'verbose':
+            args.append(f'{attr.name}={attr.value!r}' if mode == 'kwd' else repr(attr.value))
         else:
             mode = 'kwd' # skip a parameter, force keyword mode
     return cls.__name__ + '(' + ', '.join(args) + ')'
