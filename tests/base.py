3be734186d3cb7c0a88ea8ebd15c484e63725a41
Sean MacAvaney <sean.macavaney@gmail.com>
2025-09-03 13:55:45 +0100

    test integration

---
+++ tests/base.py
@@ -0,0 +1,48 @@
+import unittest
+import os
+import tempfile
+import shutil
+import pyterrier as pt
+import pytest
+
+class BaseTestCase(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(BaseTestCase, self).__init__(*args, **kwargs)
+        self.here = os.path.dirname(os.path.realpath(__file__))
+        if not pt.java.started():
+            terrier_version = os.environ.get("TERRIER_VERSION", None)
+            terrier_helper_version = os.environ.get("TERRIER_HELPER_VERSION", None)
+
+            # display for debugging what is being used - the env-vars are picked up in pt/terrier/java.py
+            if terrier_version is not None:
+                print("Testing with Terrier version " + terrier_version)
+            if terrier_helper_version is not None:
+                print("Testing with Terrier Helper version " + terrier_helper_version)
+            
+            pt.java.set_log_level("DEBUG")
+            # pt.java.add_option('-ea') can be added here to ensure that all Java assertions are met
+            pt.java.init()
+
+    def skip_windows(self):
+        if pt.utils.is_windows():
+            self.skipTest("Test disabled on Windows")
+
+
+class TempDirTestCase(BaseTestCase):
+    def setUp(self):
+        self.test_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        try:
+            shutil.rmtree(self.test_dir)
+        except:
+            pass
+
+
+def ensure_deprecated(func):
+    def wrapper(*args):
+        with pytest.deprecated_call():
+            return func(*args)
+    return wrapper
+
+parallel_test = unittest.skipIf(os.environ.get("PARALLEL_TESTING") is None, "Parallel test disabled, enable with PARALLEL_TESTING=1")
