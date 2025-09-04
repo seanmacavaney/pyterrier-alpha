355ec95cf80f0b97787596b24b4d8aa43600ec94
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2025-09-04 20:52:59 +0100

    fix sphinx error

--- pyterrier_alpha/inspect.py
+++ pyterrier_alpha/inspect.py
@@ -42,8 +42,7 @@ class ProvidesTransformerOutputs(Protocol):
     inspect the behavior is undesireable, e.g., if calling the transformer is expensive.
 
     .. code-block:: python
-        :caption: Example ``transform_output`` function, implementing
-        :class:`~pyterrier_alpha.inspect.ProvidesTransformerOutputs`.
+        :caption: Example ``transform_output`` function, implementing :class:`~pyterrier_alpha.inspect.ProvidesTransformerOutputs`.
 
         class MyRetriever(pt.Transformer):
 
